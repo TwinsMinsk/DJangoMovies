d5d169e00b629ae2a4ef5e9d94bc31a991763b21
TwinsMinsk <twinsminsk@yandex.ru>
2021-11-29 17:59:54 +0300

    Настроен шаблоны и наследование

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
-class MoviesView(View):
+class MoviesView(ListView):
     '''Список фильмов'''
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request,'movies/movies.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
-    '''Список фильмов'''
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request,'movies/movie_detail.html', {'movie': movie})
+class MovieDetailView(DetailView):
+    '''Полное описание фильма'''
+    model = Movie
+    slug_field = 'url'
